32bd80188f41c16567ab7c50b35323d93a459749
Zhu Jinhui <zhujinhui@zhangyue.com>
2018-05-30 11:19:11 +0800

    Update : update http client use namespace instead

--- src/Library/Http.php
+++ src/Library/Http.php
@@ -1,5 +1,5 @@
 <?php
- namespace Qii\Library;
+namespace Qii\Library;
 /**
  * A parallel HTTP client written in pure PHP
  *
@@ -9,20 +9,17 @@
  * @link http://hightman.cn
  * @copyright Copyright (c) 2015 Twomice Studio.
  */
-
-_require(array(
-		 __DIR__ . '/Third/hightman/ParseInterface.php',
-		__DIR__ . '/Third/hightman/HeaderTrait.php',
-		__DIR__ . '/Third/hightman/Client.php',
-		__DIR__ . '/Third/hightman/Connection.php',
-		__DIR__ . '/Third/hightman/Response.php',
-		__DIR__ . '/Third/hightman/Request.php',
-		__DIR__ . '/Third/hightman/Processor.php'
-	)
-);
+\Qii\Autoloader\Psr4::getInstance()
+    ->setUseNamespaces([
+        ['hightman\http', true],
+    ])
+    ->addNamespaces([
+        ['hightman\http', Qii_DIR . DS .'Library'. DS . 'Third'. DS .'hightman'],
+    ]);
 use hightman\http\Client;
 use hightman\http\Request;
 use hightman\http\Response;
+
 class Http extends Client
 {
 	
