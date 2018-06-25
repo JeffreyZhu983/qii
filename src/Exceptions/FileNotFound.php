9d4faab5c23579291f30545eb3e10de7f6ff4381
Zhu Jinhui <zhujinhui@zhangyue.com>
2018-06-25 10:18:37 +0800

    文件未找到的提示中替换跟路径

--- src/Exceptions/FileNotFound.php
+++ src/Exceptions/FileNotFound.php
@@ -5,7 +5,16 @@ class FileNotFound extends Errors
 {
 	const VERSION = '1.2';
 
-	/**
+	public function __construct($message, $code, $previous = null)
+    {
+        if($code == 404) {
+            $documentRoot = str_replace(array('/', '\\'), array(DS, DS), $_SERVER['DOCUMENT_ROOT']);
+            $message = str_replace($documentRoot, '', $message);
+        }
+        parent::__construct($message, $code, $previous);
+    }
+
+    /**
 	 * 显示错误
 	 *
 	 * @param Object $e Exception
